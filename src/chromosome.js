2d4a81c26165e65606e0c7fc55380a2b3f4ef049
wiktor <wiktorpiotrowski@gmail.com>
2013-03-19 16:15:19 +0100

    new class chromosome moved from moon.js

---
+++ src/chromosome.js
@@ -0,0 +1,8 @@
+
+var chromosome = function(beta, rp, eta, T){
+	
+	this.beta = beta;
+	this.rp = rp;
+	this.eta = eta;
+	this.T = T;
+}
--- src/moon.js
+++ src/moon.js
@@ -17,11 +17,3 @@ var moon = function(name, a, e, i, LAN, w, M, radius, mu, model){
 	this.locked = new Array(32);
 	this.model = model;
 }
-
-var chromosome = function(beta, rp, eta, T){
-	
-	this.beta = beta;
-	this.rp = rp;
-	this.eta = eta;
-	this.T = T;
-}
